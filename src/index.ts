4c4f84c5d66f92c57f68b4231b48bfc29d5fa02a
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-12-16 15:34:19 -0300

    add: typescript, express n ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Executei");
+import { Request, Response, NextFunction } from "express";
+import express from "express";
+
+const app = express();
+
+app.get(
+  "/status",
+  (request: Request, response: Response, next: NextFunction) => {
+    response.send({ status: "online" });
+  }
+);
+
+app.listen(3000, () => {
+  console.log("App running at port 3000");
+});
